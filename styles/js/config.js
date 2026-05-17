1094bfaa773e9642122468645541c3aa5e37525f
﹙　soul﹒ ୨୧ <throughhades@gmail.com>
2026-05-17 09:00:43 -0500

    Update config.js

--- styles/js/config.js
+++ styles/js/config.js
@@ -44,7 +44,7 @@ charadex.sheet = {
 
     designTypes: ['All', 'Official Design', 'Guest Design', 'MYO Slot', 'MYO Design', 'Mascot'],
     statuses: ['All', 'Resell', 'Trade', 'Gift', 'Voided'],
-    rarity: ['All', 'Cavernborn', 'Moonborn', 'Veilborn', 'Illegal'],
+    rarity: ['All', 'Cavernborn', 'Moonborn', 'Veilborn', 'Illegal', 'Item'],
     species: ['All', 'Sacrayne'],
     itemTypes: ['All', 'Currency', 'MYO Slot', 'Pet', 'Trait', 'Misc'],
     traitTypes: ['All', 'Ears', 'Eyes', 'Body', 'Limbs', 'Tails', 'Misc', 'Mutations']
